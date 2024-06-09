5075f31a57a3e921e2119f5e39f1a898e64faa38
Drfinik <freelanceap@yahoo.com>
2024-06-09 16:29:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const fileUpload = document.querySelector('.winning__form-file');
+        const fileInput = document.getElementById('file');
+
+        fileUpload.addEventListener('click', () => {
+            fileInput.click();
+        });
